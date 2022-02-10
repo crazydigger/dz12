d5b07ae6ec86877b6985a163c5bb2bd71f2ba2a7
borovirp <40pobGsb>
2022-02-10 13:47:38 +0300

    new update

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 url = 'http://api.hh.ru/vacancies'
 params = {'text': 'Python developer',
-'page': 1}
+'page': 1,
+'array':1,
+'User-Agent': 'api-test-agent'}
 result = requests.get(url, params=params, verify=False).json()
 items=result['items']
 first = items[0]
-
-print('URL:::::::::::::::::::',first['url'])
+req=first['snippet']
+print('URL:::::::::::::::::::',req)
 print('len',len(items),items[0])
-
-print(':::::::::::::::::::::::::::::::',result)
+reqs=[]
+for i in first['snippet']:
+    r =i['snippet']
+    for j in r:
+        print(r,'Требования!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',j)
+        reqs.append(j)
+print('Требования:::::::::::::::::::::::::::::::',reqs)
