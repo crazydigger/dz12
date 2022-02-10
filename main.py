df619b6ff79beb496190f063e11f7bd43b61a094
borovirp <40pobGsb>
2022-02-10 12:50:50 +0300

    new update

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import requests
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 url = 'http://api.hh.ru/vacancies'
-params = {'text': 'java developer',
+params = {'text': 'Python developer',
 'page': 1}
 result = requests.get(url, params=params, verify=False).json()
+items=result['items']
+first = items[0]
+
+print('URL:::::::::::::::::::',first['url'])
+print('len',len(items),items[0])
 
 print(':::::::::::::::::::::::::::::::',result)
