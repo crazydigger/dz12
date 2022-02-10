a5b8f3cd7e625858ac923d3f00ac62ea871bda3f
borovirp <40pobGsb>
2022-02-10 17:56:57 +0300

    new update

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import requests
 import ssl
+
 ssl._create_default_https_context = ssl._create_unverified_context
 url = 'http://api.hh.ru/vacancies'
 params = {'text': 'Python developer',
-'page': 1,
-'array':1,
-'User-Agent': 'api-test-agent'}
+          'page': 1,
+          'array': 1,
+          'User-Agent': 'api-test-agent'}
 result = requests.get(url, params=params, verify=False).json()
-items=result['items']
+items = result['items']
 first = items[0]
-req=first['snippet']
-print('URL:::::::::::::::::::',req)
-print('len',len(items),items[0])
-reqs=[]
-for i in first['snippet']:
-    r =i['snippet']
-    for j in r:
-        print(r,'Требования!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',j)
+req = first['snippet']
+req = str(req).split(',')
+print('URL:::::::::::::::::::', req)
+print('len', len(items), items[0])
+reqs = []
+for i in str(first['snippet']).split():
+    r = str(i)
+    for j in i:
+        print(r, 'Требования!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', j)
         reqs.append(j)
-print('Требования:::::::::::::::::::::::::::::::',reqs)
+print('Требования:::::::::::::::::::::::::::::::', reqs)
